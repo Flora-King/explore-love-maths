dd0f268e169f5572581d58d760f01dd16c9b80c8
Flora-King <giflona@yahoo.co.uk>
2023-01-18 23:50:48 +0000

    add all code for division challenge

--- assets/js/style.js
+++ assets/js/style.js
@@ -37,13 +37,16 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+
     }else if (gameType === "multiply") {
-        
         displayMultiplyQuestion(num1, num2);
+
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    }
-    else {
+
+    }else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    }else {
         alert('Unknown game type: ${gameType}');
         throw 'Unknown game type: ${gameType}. Aborting!';
     }
@@ -85,6 +88,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     }else if(operator === "-") {
         return [operand1 - operand2, "subtract"];
+    }else if(operator === "/") {
+        return [operand1 / operand2, "division"];
     }else {
         alert ('unimplemented operator ${operator}');
         throw 'unimplemented operator ${operator}.Aborting!';
@@ -128,7 +133,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivideQuestion() {
+function displayDivisionQuestion(operand1, operand2) {
+
+	document.getElementById("operand1").textContent = (operand1 * operand2);
+	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operator").textContent = "/";
 
 }
 
